581b6c52cbbfe9434ea5a3eecbf1fcc718eb5134
panchaltrilok786coder <panchaltrilok786@gmail.com>
2026-05-02 19:25:01 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ import { createUserWithEmailAndPassword , signInWithEmailAndPassword  } from "ht
 import { doc , setDoc , getDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const loginBox = document.getElementById("loginBox");
-const sugnupBox = document.getElementById("signupBox");
+const signupBox = document.getElementById("signupBox");
 const loginUser = document.getElementById("loginUser");
 const loginPass = document.getElementById("loginPass");
 const signupEmail = document.getElementById("signupEmail");
@@ -11,3 +11,69 @@ const signupPass = document.getElementById("signupPass");
 const userName = document.getElementById("signupName");
 const goSignup = document.getElementById("goSignup");
 const goLogin = document.getElementById("goLogin");
+const signupBtn =  document.getElementById("signupBtn");
+const loginBtn =  document.getElementById("loginBtn");
+
+//Chabging LoginBox and SigninBox visibility
+goSignup.addEventListener("click", ()=>{
+  signupBox.classList.remove("hidden");
+  loginBox.classList.add("hidden");
+});
+
+goLogin.addEventListener("click", ()=>{
+  signupBox.classList.add("hidden");
+  loginBox.classList.remove("hidden");
+});
+
+// ======== Signup function =======
+async function signUp(email, password){
+  try{
+  const userCredential = await createUserWithEmailAndPassword(auth , email, password);
+  if(!userCredential){
+    alert("no userCredential found");
+  }
+  alert(userCredential);
+  const user = userCredential.user
+  setDoc((db, "users"),{
+    email,
+    createdAt: new Date();
+  });
+  return{success : true, userCredetial};
+  } catch(err){
+    return{success : false, error : err.message};
+    alert(err.message);
+  }
+}
+
+async function logIn(email, password){
+  try{
+    const userCredential = await signInWithEmailAndPassword(email , password);
+    return{success : true, userCredetial};
+  } catch(err){
+    return{success : false, error : err.message};
+  }
+}
+
+loginBtn.addEventListener("click" , async ()=>{
+  if(!loginUser || !loginPass){
+    alert("Please fil the full email and password")
+  }
+  const result = await logIn(loginUser, loginPass);
+  if(result.success =="true"){
+    alert("U successfully logged in");
+  }else{
+    alert("The email or password is incorrect!");
+  }
+})
+
+signupBtn.addEventListener("click" , async ()=>{
+  if(!signupEmail || !signupPass){
+    alert("Please fil the full email and password")
+  }
+  const result = await signup(loginUser, loginPass);
+  if(result.success =="true"){
+    alert("U successfully logged in");
+  }else{
+    alert("The email or password is incorrect!");
+  }
+})
