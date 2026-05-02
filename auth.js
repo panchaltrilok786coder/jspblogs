914dfcf866e7a2340e35ee3454a3e5a8bf8a2b4e
panchaltrilok786coder <panchaltrilok786@gmail.com>
2026-05-02 20:01:50 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,2 +1,3 @@
 alert("Hello auth.js");
 import { db , auth } from "./firebase.js";
+import { createUserWithEmailAndPassword , signInWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
