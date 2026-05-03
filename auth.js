c393bf6b9b4169bbb453abef257d34d7e4576cd0
panchaltrilok786coder <panchaltrilok786@gmail.com>
2026-05-03 18:09:28 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,7 +1,8 @@
 alert("Hello auth.js");
 import { db , auth } from "./firebase.js";
 import { createUserWithEmailAndPassword , signInWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { doc , setDoc , getDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { GoogleAuthProvider , signInWithRedirect , getRedirectResult } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { doc , setDoc , getDoc , serverTimestamp} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 const loginBox = document.getElementById("loginBox");
 const signupBox = document.getElementById("signupBox");
 const loginUser = document.getElementById("loginUser");
@@ -13,7 +14,7 @@ const goSignup = document.getElementById("goSignup");
 const goLogin = document.getElementById("goLogin");
 const signupBtn =  document.getElementById("signupBtn");
 const loginBtn =  document.getElementById("loginBtn");
-
+const googleSignInBtn = document.getElementById("googleSignInBtn");
 //Chabging LoginBox and SigninBox visibility
 goSignup.addEventListener("click", ()=>{
   signupBox.classList.remove("hidden");
@@ -34,22 +35,24 @@ async function signUp(email, password){
   }
   alert(userCredential);
   const user = userCredential.user
+  const userSnap = await getDoc(doc(db, "users", user.uid));
+  if(!userSnap.exists()){
   await setDoc(doc(db, "users", user.uid),{
     email,
-    createdAt: new Date()
+    createdAt: serverTimestamp()
   });
+  }
   return{success : true, userInfo :userCredential};
   } catch(err){
     alert(err.message);
     return{success : false, error : err.message};
-    alert(err.message);
   }
 }
 
 async function logIn(email, password){
   try{
     const userCredential = await signInWithEmailAndPassword(auth, email , password);
-    return{success : true, userCredetial};
+    return{success : true, userCredential};
   } catch(err){
     return{success : false, error : err.message};
   }
@@ -57,7 +60,8 @@ async function logIn(email, password){
 
 loginBtn.addEventListener("click" , async ()=>{
   if(loginUser.value == "" || loginPass.value ==""){
-    alert("Please fil the full email and password")
+    alert("Please fil the full email and password");
+    return;
   }
   const result = await logIn(loginUser.value, loginPass.value);
   if(result.success == true){
@@ -68,13 +72,39 @@ loginBtn.addEventListener("click" , async ()=>{
 })
 
 signupBtn.addEventListener("click" , async ()=>{
-  if(!signupEmail || !signupPass){
-    alert("Please fil the full email and password")
+  if(signupEmail.value == "" || signupPass.value == ""){
+    alert("Please fil the full email and password");
+    return;
   }
-  const result = await signUp(loginUser.value, loginPass.value);
+  const result = await signUp(signupEmail.value, signupPass.value);
   if(result.success == true){
-    alert("U successfully logged in");
+    alert("U successfully signedUp");
   }else{
     alert("The email or password is sincorrect!");
   }
+});
+
+// ================= Google Auth =================
+
+const provider = new GoogleAuthProvider();
+googleSignInBtn.addEventListener("click" , ()=>{
+  signInWithRedirect(auth, provider);
 })
+
+window.addEventListener("load" , async ()=>{
+    try{
+      const result = await getRedirectResult(auth);
+      if(result && result.user){
+        console.log("The user is: " , result.user);
+        const userSnap =  await getDoc(doc(db , "users", result.user.uid));
+        if(!userSnap.exists()){
+        await setDoc(doc(db , "users", result.user.uid), {
+          email: result.user.email,
+          createdAt: serverTimestamp()
+        })
+        }
+      }
+    } catch(err){
+      alert("Error :" + err.message)
+    }
+  })
